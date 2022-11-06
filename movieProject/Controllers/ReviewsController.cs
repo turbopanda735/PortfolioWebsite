7fb06e9593b971f99e9abed95588aa625594f887
Joel <franco4prez@gmail.com>
2022-11-06 12:06:04 -0500

    added pygame to homepage

---
+++ movieProject/Controllers/ReviewsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using movieProject.Models.Movie;
+using movieProject.Models.Reviews;
+
+namespace movieProject.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly IReviewsRepository repo;
+        public ReviewsController(IReviewsRepository repo)
+        {
+            this.repo = repo;
+        }
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
